21a87237e53830f75b1a62c050264204abf1d4a6
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-10-29 14:08:47 +0300

    Added more about us content

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,5 +1,9 @@
 <script>
     import aboutUsHeadingSrc from "$lib/assets/about-us/about-us-heading.webp";
+
+    import ADevineDesignLogoSrc from "$lib/assets/about-us/a-devine-design.svg";
+    import rollerBlindsLogoSrc from "$lib/assets/about-us/roller-blinds-toronto.svg";
+    import shutterBoysLogoSrc from "$lib/assets/about-us/shutter-boys.svg";
 </script>
 
 <main>
@@ -8,13 +12,21 @@
         <h1 class="fs-xl">We cover your windows with a heavenly touch</h1>
         <p>Many years ago, our founder had a vision to furnish every home in Toronto and surrounding regions with the highest quality wood shutter available on the market. 25 years to that day, and our shutters are finally filling homes across the nation! We've successfully served thousands of delighted customers, and we are ready to give you the same service, quality and price that we've always been known for.</p>
     </article>
+    <p>California Shutters Toronto is the manufacturing arm of <span class="text-yellow">A Divine Design</span>, specializing in the production of premium California shutters. With over two decades of experience, We focus on creating high-quality shutters using premium poplar wood, ensuring durability and timeless appeal. Additionally, through our sister brands, <span class="text-yellow">Shutter Boys</span> and <span class="text-yellow">Roller Blinds Toronto</span>, we offer a wide range of custom blinds and roller blinds, providing complete window covering solutions for high-end homeowners, custom home builders, and interior designers.</p>
+    <div class="logos">
+        <img src="{shutterBoysLogoSrc}" alt="shutter boys company logo">
+        <img src="{rollerBlindsLogoSrc}" alt="roller blinds company logo">
+        <img src="{ADevineDesignLogoSrc}" alt="a devine design company logo">
+    </div>
 </main>
 <div class="box"></div>
 
 <style>
     main {
         padding: var(--spacing);
-        padding-top: 6.5rem;
+        padding-top: 9.5rem;
+        max-width: 1150px;
+        margin: auto;
     }
 
     article {
@@ -24,8 +36,6 @@
         gap: var(--spacing);
         align-items: center;
         justify-items: center;
-        max-width: 1150px;
-        margin: auto;
     }
 
     article > * {
@@ -45,7 +55,41 @@
         max-width: 500px;
     }
 
+    main > p {
+        margin-block: calc(var(--spacing) * 2);
+    }
+
+    .logos {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+        gap: var(--spacing);
+    }
+
+    .logos img:nth-of-type(1) {
+        max-width: 130px;
+    }
+
+    .logos img:nth-of-type(2) {
+        max-width: 300px;
+    }
+
+    .logos img:nth-of-type(3) {
+        max-width: 200px;
+    }
+
+    @media (max-width: 700px) {
+        .logos {
+            flex-direction: column;
+        }
+    }
+    
     @media (max-width: 1150px) {
+        main {
+            padding-top: 6.5rem;
+        }
+
         article {
             grid-template-columns: 1fr;
         }
@@ -54,4 +98,5 @@
             order: -1;
         }
     }
+
 </style>
