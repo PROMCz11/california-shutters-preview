81d28d1a5809c6755ce54331854b243ba1f1c397
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-11-03 21:14:57 +0300

    Creating api for D1 testing

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,6 +1,6 @@
 import { json } from "@sveltejs/kit";
 
-export const GET = async () => {
+export const GET = async ({ platform }) => {
     const dataFromDB = await platform.env.testing.prepare("SELECT * FROM blog").all();
     if(!dataFromDB) {
         return json({ status: false });
