7a24cca9f70a2e89e3977da40bc4bb0605dd4436
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-11-03 21:11:12 +0300

    Creating api for D1 testing

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,21 +1,21 @@
-// import { json } from "@sveltejs/kit";
+import { json } from "@sveltejs/kit";
 
-// export const GET = async () => {
-//     const dataFromDB = await platform.env.DB.prepare("SELECT * FROM blog");
-//     if(!dataFromDB) {
-//         return json({ status: false });
-//     }
-//     return json({ status: true, data: dataFromDB});
-// }
-
-export async function GET({ platform }) {
-    const query = `SELECT * FROM blog;`;
-    
-    try {
-      const result = await platform.env.testing.prepare(query).all();
-      return new Response(JSON.stringify(result), { status: 200 });
-    } catch (error) {
-      console.error('Error fetching all blog entries:', error);
-      return new Response('Error fetching data: ' + error, { status: 500 });
+export const GET = async () => {
+    const dataFromDB = await platform.env.testing.prepare("SELECT * FROM blog");
+    if(!dataFromDB) {
+        return json({ status: false });
     }
-  }  
+    return json({ status: true, data: dataFromDB});
+}
+
+// export async function GET({ platform }) {
+//     const query = `SELECT * FROM blog;`;
+    
+//     try {
+//       const result = await platform.env.testing.prepare(query).all();
+//       return new Response(JSON.stringify(result), { status: 200 });
+//     } catch (error) {
+//       console.error('Error fetching all blog entries:', error);
+//       return new Response('Error fetching data: ' + error, { status: 500 });
+//     }
+//   }
