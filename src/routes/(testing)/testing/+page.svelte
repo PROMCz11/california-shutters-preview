c9ccfbbb03bcaa63f94a4c1c5655e17a0de7358b
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-11-04 17:20:45 +0300

    Creating a testing page

---
+++ src/routes/(testing)/testing/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    const fetchAPI = async () => {
+        const res = await fetch("../api");
+        const json = await res.json();
+        console.log(json);
+    }
+</script>
+
+<main>
+    <p>Check the console</p>
+    <button on:click={fetchAPI}>Fetch /api</button>
+</main>
+
+<style>
+    main {
+        padding: .5rem;
+    }
+</style>
