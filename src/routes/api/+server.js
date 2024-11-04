2cf8a6ffea5f5cda554040cac9ec972fee3ace36
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-11-04 16:56:15 +0300

    HELLO3

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,11 +1,3 @@
-// export async function GET({ platform }) {   
-//     const { results } = await platform.env.DB.prepare("SELECT * FROM blogs").all();
-//     return new Response(JSON.stringify(results), {
-//       headers: { "content-type": "application/json" }
-//     });
-//   }
-  
-
 import { json } from "@sveltejs/kit";
 
 export const GET = async ({ platform }) => {
