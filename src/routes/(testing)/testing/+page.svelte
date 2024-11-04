e3aea72fed48f769c774a78c408fd78c59f6cc7f
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-11-04 17:26:09 +0300

    Upgrading the testing page

--- src/routes/(testing)/testing/+page.svelte
+++ src/routes/(testing)/testing/+page.svelte
@@ -1,14 +1,32 @@
 <script>
-    const fetchAPI = async () => {
-        const res = await fetch("../api");
-        const json = await res.json();
-        console.log(json);
+    // const fetchAPI = async () => {
+    //     status = "No data yet";
+    //     const res = await fetch("../api");
+    //     const json = await res.json();
+    //     console.log(json);
+    //     status = "Data recieved, check the console";
+    // }
+
+    const fetchAPI = () => {
+        status = "No data yet";
+        fetch("../api")
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+            status = "Data recieved, check the console";
+        })
+        .catch(err => {
+            console.log(err);
+            status = "Error, check the console";
+        })
     }
+
+    let status = "No data yet"
 </script>
 
 <main>
-    <p>Check the console</p>
     <button on:click={fetchAPI}>Fetch /api</button>
+    <p>Status: {status}</p>
 </main>
 
 <style>
