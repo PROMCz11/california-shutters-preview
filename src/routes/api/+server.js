2c4d19558ef2ec811da6ad28be113705b5b5461c
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-11-04 16:41:16 +0300

    Trying the api

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,7 +1,14 @@
-export async function GET({ platform }) {   
-    const { results } = await platform.env.DB.prepare("SELECT * FROM blogs").all();
-    return new Response(JSON.stringify(results), {
-      headers: { "content-type": "application/json" }
-    });
-  }
+// export async function GET({ platform }) {   
+//     const { results } = await platform.env.DB.prepare("SELECT * FROM blogs").all();
+//     return new Response(JSON.stringify(results), {
+//       headers: { "content-type": "application/json" }
+//     });
+//   }
   
+
+import { json } from "@sveltejs/kit";
+
+export const GET = async ({ platform }) => {
+  const results = await platform.env.DB.prepare("SELECT * FROM blogs").all();
+  return json({ data: results });
+}
