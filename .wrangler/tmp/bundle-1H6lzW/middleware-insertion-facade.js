32d43286a96d6165bbf8439db0055d2c64d58a9b
zain-shaban <zainshaban25@gmail.com>
2024-11-04 16:32:31 +0300

    update data base

---
+++ .wrangler/tmp/bundle-1H6lzW/middleware-insertion-facade.js
@@ -0,0 +1,10 @@
+				import worker, * as OTHER_EXPORTS from "E:\\web-projects\\california-website\\src\\index.js";
+				import * as __MIDDLEWARE_0__ from "E:\\web-projects\\california-website\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+
+				export * from "E:\\web-projects\\california-website\\src\\index.js";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default
+				]
+				export default worker;
