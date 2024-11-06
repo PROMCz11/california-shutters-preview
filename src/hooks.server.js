5702c6f03dd10e3bd9f8e3ebe28555f1fa2fab78
zain-shaban <zainshaban25@gmail.com>
2024-11-06 13:12:40 +0300

    fix hooks

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,31 +1,33 @@
 import jwt from '@tsndr/cloudflare-worker-jwt';
 import { JWT_SECRET } from '$env/static/private';
 import { json } from '@sveltejs/kit';
+//import { redirect } from '@sveltejs/kit';
+
+const protectedRoutes = ['/api/blogs/create', '/api/blogs/update', '/api/blogs/delete'];
 
 export async function handle({ event, resolve }) {
-	if (
-		event.url.pathname.startsWith('/api/blogs/create') ||
-		event.url.pathname.startsWith('/api/blogs/update') ||
-		event.url.pathname.startsWith('/api/blogs/delete')
-	) {
+	if (protectedRoutes.some((route) => event.url.pathname.startsWith(route))) {
+		const sessionToken = event.cookies.get('sessionToken');
+		if (!sessionToken) {
+			return json({ status: false, message: 'invalid token' });
+		}
+
 		try {
-			if (event.cookies.get('sessionToken')) {
-				const authToken = event.cookies.get('sessionToken').split(' ')[1];
-				const { payload } = await jwt.verify(authToken, JWT_SECRET);
-				if (
-					(await event.platform.env.DB.prepare(
-						`SELECT * FROM admins where email="${payload.email}"`
-					).all()).results
-				) {
-					const response = await resolve(event);
-					return response;
-				}
+			const authToken = sessionToken.split(' ')[1];
+			const { payload } = await jwt.verify(authToken, JWT_SECRET);
+			const adminExists = (
+				await event.platform.env.DB.prepare(`SELECT * FROM admins WHERE email = ?`)
+					.bind(payload.email)
+					.all()
+			).results;
+			if (adminExists) {
+				return await resolve(event);
 			}
 			throw new Error('invalid token');
 		} catch {
 			return json({ status: false, message: 'invalid token' });
+			// TO DO
+			// redirect
 		}
 	}
-	const response = await resolve(event);
-	return response;
 }
