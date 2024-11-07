18db59deb96bcfde8291153792049402bd283400
zain-shaban <zainshaban25@gmail.com>
2024-11-08 00:04:06 +0300

    fix error

--- src/routes/api/blogs/+server.js
+++ src/routes/api/blogs/+server.js
@@ -3,7 +3,7 @@ import { json } from '@sveltejs/kit';
 export const GET = async ({ platform }) => {
 	try {
 		let allBlogs = (await platform.env.DB.prepare(`SELECT * FROM blogs`).all()).results;
-		return json({ status: true, data: { allBlogs:JSON.parse(allBlogs) } });
+		return json({ status: true, data: { allBlogs } });
 	} catch (error) {
 		return json({ status: false, message: error.message });
 	}
