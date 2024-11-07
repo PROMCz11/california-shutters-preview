46d2a0cc5a7672d62766c1fc31840902eeb5d423
zain-shaban <zainshaban25@gmail.com>
2024-11-07 23:59:52 +0300

    fix blogs

--- src/routes/api/blogs/[id]/+server.js
+++ src/routes/api/blogs/[id]/+server.js
@@ -3,9 +3,9 @@ import { json } from '@sveltejs/kit';
 export const GET = async ({ platform, params }) => {
 	try {
 		const { id } = params;
-		let allBlogs = (await platform.env.DB.prepare(`SELECT * FROM blogs where id = ${id}`).all())
+		let blogById = (await platform.env.DB.prepare(`SELECT * FROM blogs where id = ${id}`).all())
 			.results;
-		return json({ status: true, data: { allBlogs } });
+		return json({ status: true, data: { blog:blogById.blog } });
 	} catch (error) {
 		return json({ status: false, message: error.message });
 	}
