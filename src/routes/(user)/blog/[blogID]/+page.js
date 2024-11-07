eab9e4711fb9109576072c2230c2609f9db78f1e
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-11-08 02:10:27 +0300

    Testing json parse

--- src/routes/(user)/blog/[blogID]/+page.js
+++ src/routes/(user)/blog/[blogID]/+page.js
@@ -2,11 +2,12 @@ export const load = async ({ fetch, params }) => {
     const blogID = params.slug;
     const res = await fetch(`../../../api/blogs/${blogID}`);
     const blogData = await res.json();
+    console.log(blogData);
+    console.log("working");
     const status = blogData.status;
     if(!status) {
         return;
     }
     const blog = JSON.parse(blogData.data.blog);
-    console.log(blog);
     return {blog};
 }
