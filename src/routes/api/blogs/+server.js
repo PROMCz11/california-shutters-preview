4a68fccf91bb15bbea60a155dbbc543228c5d9aa
zain-shaban <zainshaban25@gmail.com>
2024-11-08 00:20:32 +0300

    fix some bugs

--- src/routes/api/blogs/+server.js
+++ src/routes/api/blogs/+server.js
@@ -3,7 +3,11 @@ import { json } from '@sveltejs/kit';
 export const GET = async ({ platform }) => {
 	try {
 		let allBlogs = (await platform.env.DB.prepare(`SELECT * FROM blogs`).all()).results;
-		return json({ status: true, data: { allBlogs } });
+		const transformedBlogs = allBlogs.map(item => ({
+			...item,
+			blog: JSON.parse(item.blog)
+		  }));
+				  return json({ status: true, data: { allBlogs:transformedBlogs } });
 	} catch (error) {
 		return json({ status: false, message: error.message });
 	}
