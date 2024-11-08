da3e052305ef756df6bd93ec81959aa130844473
zain-shaban <zainshaban25@gmail.com>
2024-11-08 23:18:49 +0300

    get recent blogs

--- src/routes/api/blogs/[id]/+server.js
+++ src/routes/api/blogs/[id]/+server.js
@@ -5,7 +5,21 @@ export const GET = async ({ platform, params }) => {
 		const { id } = params;
 		let blogById = (await platform.env.DB.prepare(`SELECT * FROM blogs where blogID = ${id}`).all())
 			.results[0];
-		return json({ status: true, data: { blog:blogById.blog} });
+		let recentBlogs = (await platform.env.DB.prepare(`SELECT * FROM blogs ORDER BY time DESC LIMIT 2`).all()).results;
+		recentBlogs = recentBlogs.map((item) => ({
+			...item,
+			blog: JSON.parse(item.blog)
+		}));
+		recentBlogs = recentBlogs.map((item) => {
+			let newItem = {};
+			newItem.blogID = item.blogID;
+			newItem.title = item.blog.blogTitle;
+			newItem.date = item.blog.rawBlog.time;
+			newItem.blogImgURL = item.blog.blogImgURL;
+			newItem.blogImgDesc = item.blog.blogImgDesc;
+			return newItem;
+		});
+		return json({ status: true, data: { blog: blogById.blog,recentBlogs } });
 	} catch (error) {
 		return json({ status: false, message: error.message });
 	}
