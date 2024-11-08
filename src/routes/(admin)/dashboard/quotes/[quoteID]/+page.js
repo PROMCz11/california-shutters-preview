e5bca38dcfa64414da6adeeb511e1cefefd951db
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-11-08 17:58:19 +0300

    Configuring navigation to /quotes/[quoteID]

--- src/routes/(admin)/dashboard/quotes/[quoteID]/+page.js
+++ src/routes/(admin)/dashboard/quotes/[quoteID]/+page.js
@@ -3,6 +3,7 @@ export const load = async ({ fetch, params }) => {
     console.log(quoteID);
     const res = await fetch(`../../../../api/quotes/${quoteID}`);
     const quoteData = await res.json();
+    console.log(quoteData);
     const status = quoteData.status;
     if(!status) {
         console.log("didn't work", quoteData.message);
