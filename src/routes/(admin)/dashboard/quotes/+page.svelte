7c5e5d64d5e461b1b708727e8f5ab4f1ed65190d
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-11-08 21:23:21 +0300

    Fixing quotes navigation

--- src/routes/(admin)/dashboard/quotes/+page.svelte
+++ src/routes/(admin)/dashboard/quotes/+page.svelte
@@ -37,10 +37,9 @@
         <!-- svelte-ignore a11y_no_static_element_interactions -->
         <!-- svelte-ignore a11y_click_events_have_key_events -->
             <div class="quote" class:seen id="{quoteID}" on:click={e => {
-                if(e.target.tagName === "input" || e.target.classList.contains("call-btn") || e.target.classList.contains("email-btn")) {
-                    return;
+                if(!(e.target.tagName === "input" || e.target.classList.contains("call-btn") || e.target.classList.contains("email-btn"))) {
+                    goto(`quotes/${quoteID}`);
                 }
-                goto(`quotes/${quoteID}`);
             }}>
                 <input type="checkbox" bind:checked>
                 <p class="name">{name}</p>
